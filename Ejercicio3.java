90c0948a455efb9ef0c6ff1e0f10a9a965eb9c78
Carlos Garcia Gelabert <carlosgarciagelabert1990@gmail.com>
2015-12-03 00:03:51 +0100

    Ejercicio Java

---
+++ Ejercicio3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ejercicio3{
+	public static void main (String [] args){
+		Scanner entrada=new Scanner(System.in);
+		System.out.println("Introduce un precio");
+		int precio=entrada.nextInt();
+		if (precio>80){
+			precio=precio-(precio*10/100);
+			System.out.println(precio);
+		}
+		if(precio<5){
+			precio=precio+2;
+			System.out.println("Precio + gastos de gestion " + precio);
+		}
+	}
+}
